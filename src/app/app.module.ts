2f21a791a623093ed15d53e24ba727912fa1e449
Luis Antonio Feregrino <djlaf2008@hotmail.com>
2019-10-19 14:51:34 -0500

    Firebase configuration added in enviroments and app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
